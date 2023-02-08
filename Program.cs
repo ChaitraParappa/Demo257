951f1a24eb20a727f9f8c0a0c7b08729c62be7a5
ChaitraParappa <chaitra.p321@gmail.com>
2023-02-08 07:40:43 +0530

    First file commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Username.DisplayUsername();
+
+            //Console.WriteLine(Username.a);
+
+            FlipCoin.CalculatePercentage();
+
+
+
+
+        }
+    }
+
+}
